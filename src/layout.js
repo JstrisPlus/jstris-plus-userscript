d8ab17230fceb627fffa55f910eeae47648afe78
orznull <tacothing@gmail.com>
2025-04-21 20:54:03 -0400

    bg refactor

--- src/layout.js
+++ src/layout.js
@@ -1,10 +1,18 @@
 import { Config } from "./config"
 
+const createBGElement = () => {
+  const ele = document.createElement("div");
+  ele.id = "JS_PLUS_BG_ELEMENT";
+  ele.style = "z-index: -100; position: absolute; height: max(100%, 100vh); width: 100%; top: 0px; left: 0px; background-size: cover;";
+  document.body.appendChild(ele)
+  return ele;
+}
+
 const changeBG = link => {
-  console.log("Changing BG to "+link);
-  var app = document.getElementById("BG_only");
+  console.log("Changing BG to " + link);
+  document.body.style.position = "relative"
+  var app = document.getElementById("JS_PLUS_BG_ELEMENT") ?? createBGElement();
   app.style.backgroundImage = `url(${link})`;
-  app.style.backgroundSize = 'cover'
 }
 
 export const initLayout = () => {
